f5aee21eaa214e08615b316ce85a208461f1944a
Rafael Carvalho J. Araujo <rrafaelcarvalhoj@gmail.com>
2023-12-31 11:00:58 -0300

    feat: Main section

--- jorgefelipefit/src/components/Main.tsx
+++ jorgefelipefit/src/components/Main.tsx
@@ -1,15 +1,25 @@
 import { Button } from "@material-tailwind/react";
+import { styled } from "styled-components";
+
+const Section = styled.section`
+    display: flex;
+    max-width: 1320px;
+    margin: 0px auto;
+    height: 80vh;
+    paidding: 0px 20px;
+`;
+
 export default function Main() {
 
     return (
-        <div className="flex max-w-screen-2xl mx-auto px-2 h-96 bg-red-500">
-            <div className="my-auto">
-                <h1 className="font-bold uppercase text-6xl">Inspire-se</h1>
-                <h3>A mudança que você quer está na decisão que você toma</h3>
-                <Button placeholder="" className="mt-4">
-                    Button
+        <Section>
+            <div className="mt-64 mx-auto lg:mx-10">
+                <h1 className="font-bold uppercase lg:text-left text-6xl text-center">Inspire-se</h1>
+                <h3 className="text-center lg:text-left">A mudança que você quer está na decisão que você toma</h3>
+                <Button placeholder="" className="block mt-4 mx-auto lg:mx-2 text-normal text-gray-200 transition duration-300 transform hover:-translate-y-1">
+                    SAIBA MAIS
                 </Button>
             </div>
-        </div>
+        </Section>
     );
 }
